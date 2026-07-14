70d5cb496780cb53dc7b571e16f893fbf80e22a9
Krunal Parmar <KRUMER2023@gmail.com>
2026-07-14 21:24:37 +0530

    [add][14-7-26] Node

---
+++ NPMWorking/app.js
@@ -0,0 +1,3 @@
+// let add = require("./logic")
+import add from './logic.js'
+console.log(add(10,20));
---
+++ NPMWorking/try.js
@@ -0,0 +1,15 @@
+// give all global object as per env
+// like node.js it shows of node env
+//  if in browser console show error
+//  using code runner show its env
+// console.log(global);
+
+
+console.log(process.exit);
+console.log("------------------------------------");
+console.log(__dirname);
+console.log("------------------------------------");
+console.log(__filename);
+console.log("------------------------------------");
+
+
